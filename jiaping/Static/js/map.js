14dae932a253541a04cb6c9e8e3b0538bceb264f
choushuiguo325 <juzituo325@gmail.com>
2020-11-17 21:37:58 -0600

    updated the map with division filters and marker icons

--- jiaping/Static/js/map.js
+++ jiaping/Static/js/map.js
@@ -2,7 +2,6 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = 'https://raw.githubusercontent.com/DanielKarpowicz/final_project_plays_prediction/jiaping/jiaping/Resources/team_info.json';
 var queryUrl2 = 'https://raw.githubusercontent.com/DanielKarpowicz/final_project_plays_prediction/jiaping/jiaping/Resources/team_info.geojson'
-
 var myMap = L.map("map", {
     center: [35.52, -100.67],
     zoom: 4
@@ -46,69 +45,35 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 // });
 
 
-
-// L.control.layers(baseMaps, overlayMaps,  {
-//     collapsed: false
-// }).addTo(myMap);
-
-// // define a function to process information of each team
-// function teamProcess(teams, layer) {
-//     console.log("process team", teams);
-//     teams.forEach((team) => {
-//         var conference = team.conference;
-//         var club = team.club;
-//         var city = team.city;
-//         var division = team.division;
-//         var capacity = team.capacity;
-//         var abbr = team.abbr;
-//         var stadium = team.stadium;
-//         var lon = team.lon;
-//         var lat = team.lat;
-//         var head_coach = team.head_coach;
-//         var logo_address = team.logo_address;
-//         var icon = L.icon({
-//             iconUrl: logo_address,
-//             iconSize: [30, 30]
-//         });
-
-//         var marker = L.marker([lat, lon], {icon: icon }).addTo(myMap);
-//         marker.bindPopup(`<h2>${club}</h2><hr>
-//         <h3>
-//         Head Coach: ${head_coach}<br>
-//         Confercence: ${conference}<br>
-//         Division: ${division}<br>
-//         City: ${city}<br>
-//         Statium: ${stadium}<br>
-//         Capacity: ${capacity}
-//         </h3>`).addTo(layer);
-//     });
-
-// };
-
-// // access the data in stored in the team info json API
-// d3.json(queryUrl).then(function (teams) {
-//     ACFteamsInfo = teams.filter(team => team.conference === 'American Football Conference');
-//     NCFteamsInfo = teams.filter(team => team.conference === 'National Football Conference');
-//     console.log('ACFteamsInfo', ACFteamsInfo);
-//     console.log('NCFteamsInfo', NCFteamsInfo);
-
-//     teamProcess(ACFteamsInfo, ACFlayer);
-//     teamProcess(NCFteamsInfo, NCFlayer);
-// });
-
-
-
-// added divison filters
-// geojson
 let checkboxStates
 
 d3.json(queryUrl2).then(function (teams) {
 
-    const geojsonLayer = L.geoJSON(null, {
+    var geojsonLayer = L.geoJSON(null, {
+        pointToLayer: (feature, latlng) => {
+            var myIcon = L.icon({
+                iconUrl: feature.properties.logo_address,
+                iconSize: [32, 32],
+            });
+            var coordinates = feature.geometry.coordinates;
+            return L.marker(latlng, { icon: myIcon });
+        },
+
         filter: (feature) => {
             const isDivisionChecked = checkboxStates.divisions.includes(feature.properties.division)
             const isConferenceChecked = checkboxStates.conferences.includes(feature.properties.conference)
             return isDivisionChecked && isConferenceChecked //only true if both are true
+        },
+        onEachFeature: (feature,layer) => {
+            layer.bindPopup(`<h2>${feature.properties.club}</h2><hr>
+            <h3>
+            Head Coach: ${feature.properties.head_coach}<br>
+            Confercence: ${feature.properties.conference}<br>
+            Division: ${feature.properties.division}<br>
+            City: ${feature.properties.city}<br>
+            Statium: ${feature.properties.stadium}<br>
+            Capacity: ${feature.properties.capacity}
+            </h3>`);
         }
     }).addTo(myMap)
 
@@ -127,7 +92,6 @@ d3.json(queryUrl2).then(function (teams) {
             };
         }
     };
-
 
     for (let input of document.querySelectorAll('input')) {
         //Listen to 'change' event of all inputs
@@ -152,126 +116,12 @@ d3.json(queryUrl2).then(function (teams) {
 
 
 
-// json 
-// function updateCheckboxStates() {
-//     checkboxStates = {
-//         divisions: [],
-//         conferences: []
-//     }
-
-//     for (let input of document.querySelectorAll('input')) {
-//         if (input.checked) {
-//             switch (input.className) {
-//                 case 'conference': checkboxStates.conferences.push(input.value); break
-//                 case 'division': checkboxStates.divisions.push(input.value); break
-//             }
-//         }
-//     }
-// };
-
-// function filtration(team) {
-//     const isDivisionChecked = checkboxStates.divisions.includes(team.division)
-//     const isConferenceChecked = checkboxStates.conferences.includes(team.conference)
-//     return isDivisionChecked && isConferenceChecked //only true if both are true
-// };
-
-// function teamProcess(teams, layer) {
-//     console.log("process team", teams);
-//     teams.forEach((team) => {
-//         var conference = team.conference;
-//         var club = team.club;
-//         var city = team.city;
-//         var division = team.division;
-//         var capacity = team.capacity;
-//         var abbr = team.abbr;
-//         var stadium = team.stadium;
-//         var lon = team.lon;
-//         var lat = team.lat;
-//         var head_coach = team.head_coach;
-//         var logo_address = team.logo_address;
-//         var icon = L.icon({
-//             iconUrl: logo_address,
-//             iconSize: [30, 30]
-//         });
-
-//         var marker = L.marker([lat, lon], {icon: icon }).addTo(myMap);
-//         marker.bindPopup(`<h2>${club}</h2><hr>
-//         <h3>
-//         Head Coach: ${head_coach}<br>
-//         Confercence: ${conference}<br>
-//         Division: ${division}<br>
-//         City: ${city}<br>
-//         Statium: ${stadium}<br>
-//         Capacity: ${capacity}
-//         </h3>`).addTo(layer);
-//     });
-
-// };
 
 
-// d3.json(queryUrl).then(function (teams) {
-//     teams.forEach((team) => {
-//         var conference = team.conference;
-//         var club = team.club;
-//         var city = team.city;
-//         var division = team.division;
-//         var capacity = team.capacity;
-//         var abbr = team.abbr;
-//         var stadium = team.stadium;
-//         var lon = team.lon;
-//         var lat = team.lat;
-//         var head_coach = team.head_coach;
-//         var logo_address = team.logo_address;
-//         var icon = L.icon({
-//             iconUrl: logo_address,
-//             iconSize: [30, 30]
-//         });
 
-//         if (filtration(team)) {
-//             var marker = L.marker([lat, lon], { icon: icon })
-//             var geojsonLayer = marker.addTo(myMap)
-//         };
-//         // var marker = L.marker([lat, lon], {icon: icon},{
-//         //     filter: (team) => {
-//         //         const isDivisionChecked = checkboxStates.divisions.includes(team.division)
-//         //         const isConferenceChecked = checkboxStates.conferences.includes(team.conference)
-//         //         return isDivisionChecked && isConferenceChecked //only true if both are true
-//         //       }
-//         // }).addTo(myMap);
 
-//         marker.bindPopup(`<h2>${club}</h2><hr>
-//         <h3>
-//         Head Coach: ${head_coach}<br>
-//         Confercence: ${conference}<br>
-//         Division: ${division}<br>
-//         City: ${city}<br>
-//         Statium: ${stadium}<br>
-//         Capacity: ${capacity}
-//         </h3>`).addTo(myMap);
-//     });
-//     // 
-//     // const geojsonLayer = L.marker(null,{
-//     //     filter: (team) => {
-//     //     const isDivisionChecked = checkboxStates.divisions.includes(team.division)
-//     //     const isConferenceChecked = checkboxStates.conferences.includes(team.conference)
-//     //     return isDivisionChecked && isConferenceChecked //only true if both are true
-//     //   }
-//     // }).addTo(myMap)
-//     // 
-//     for (let input of document.querySelectorAll('input')) {
-//         //Listen to 'change' event of all inputs
-//         input.onchange = (e) => {
-//             geojsonLayer.clearLayers()
-//             updateCheckboxStates()
-//             // geojsonLayer.addData(teams)
-//             teamProcess(teams, geojsonLayer)
-//         }
-//     }
-//     /****** INIT ******/
-//     updateCheckboxStates()
-//     // geojsonLayer.addData(teams)
-//     teamProcess(teams, geojsonLayer)
-// });
+
+
 
 
 
